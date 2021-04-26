39a7349c10e7c5e401692fa346459c56dfba2416
dullin <dullin@hololink.org>
2021-04-26 06:25:39 -0400

    2.06 pingpong

--- app.js
+++ app.js
@@ -7,26 +7,16 @@ const fs = require('fs/promises');
 
 var app = express();
 
-const directory = path.join('/', 'usr', 'src', 'app', 'files')
-const filePath = path.join(directory, 'pingpong.txt')
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-async function writepingpong() {
-    await fs.mkdir(directory, {recursive:true})
-    const logString = `Ping / Pongs: ${counter}`
-    await fs.writeFile(filePath, logString)
-  }
-
 let counter = 0;
 
 app.get('/', (req, res) => {
-  res.send(`pong ${counter}`)
+  res.send(`Ping / Pong ${counter}`)
   counter++
-  writepingpong()
 })
 
 
